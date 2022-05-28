d3d81fbf97d02cd70d1db9af498e5a6ecb4908bb
Emmanuel Daniel <emmsdan.inc@gmail.com>
2022-05-28 11:45:03 +0100

    Fix Asynchronous Compactability
    - Allow using custom parser and stringifyer instead of `JSON.parse` and `JSON.stringify`
    - Made Compatible with asynchronous storage like `localForage` and `AsycnStorage`
    
    Currently can now be used in react native project

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import { AtomEffect } from 'recoil'
 
 export interface PersistStorage {
-  setItem(key: string, value: string): void | Promise<void>
-  mergeItem?(key: string, value: string): Promise<void>
-  getItem(key: string): null | string | Promise<string>
+    setItem(key: string, value: string): void | Promise<void>
+    mergeItem?(key: string, value: string): Promise<void>
+    getItem(key: string): null | string | Promise<string>
 }
 
 export interface PersistConfiguration {
-  key?: string
-  storage?: PersistStorage
+    key?: string
+    storage?: PersistStorage
+    stringify?: (value: any) => any
+    parser?: (value: any) => any
 }
 
 /**
@@ -19,94 +21,94 @@ export interface PersistConfiguration {
  * @param config.storage Local storage to use, defaults to `localStorage`
  */
 export const recoilPersist = (
-  config: PersistConfiguration = {},
+    config: PersistConfiguration = {},
 ): { persistAtom: AtomEffect<any> } => {
-  if (typeof window === 'undefined') {
-    return {
-      persistAtom: () => {},
+    if (typeof window === 'undefined') {
+        return {
+            persistAtom: () => {},
+        }
     }
-  }
 
-  const { key = 'recoil-persist', storage = localStorage } = config
+    const { key = 'recoil-persist', storage = localStorage, stringify=JSON.stringify, parser=JSON.parse } = config
+    const persistAtom: AtomEffect<any> = ({ onSet, node, trigger, setSelf }) => {
+        if (trigger === 'get') {
+            const state = getState()
+            if (typeof state.then === 'function') {
+                state.then((s: any) => {
+                    if (s && s.hasOwnProperty(node.key)) {
+                        setSelf(s[node.key])
+                    }
+                })
+            }
 
-  const persistAtom: AtomEffect<any> = ({ onSet, node, trigger, setSelf }) => {
-    if (trigger === 'get') {
-      const state = getState()
-      if (typeof state.then === 'function') {
-        state.then((s) => {
-          if (s.hasOwnProperty(node.key)) {
-            setSelf(s[node.key])
-          }
+            if (state.hasOwnProperty(node.key)) {
+                setSelf(state[node.key])
+            }
+        }
+
+        onSet( (newValue, _, isReset) => {
+            const state = getState()
+            if (typeof state.then === 'function') {
+                state.then((s: any) => updateState(newValue, s ?? {}, node.key, isReset))
+            } else {
+                updateState(newValue, state, node.key, isReset)
+            }
         })
-      }
-      if (state.hasOwnProperty(node.key)) {
-        setSelf(state[node.key])
-      }
     }
 
-    onSet(async (newValue, _, isReset) => {
-      const state = getState()
-      if (typeof state.then === 'function') {
-        state.then((s: any) => updateState(newValue, s, node.key, isReset))
-      } else {
-        updateState(newValue, state, node.key, isReset)
-      }
-    })
-  }
+    const updateState = (
+        newValue: any,
+        state: any,
+        key: string,
+        isReset: boolean,
+    ) => {
+        if (isReset) {
+            delete state[key]
+        } else {
+            state[key] = newValue
+        }
 
-  const updateState = (
-    newValue: any,
-    state: any,
-    key: string,
-    isReset: boolean,
-  ) => {
-    if (isReset) {
-      delete state[key]
-    } else {
-      state[key] = newValue
+        setState(state)
     }
 
-    setState(state)
-  }
+    const getState = (): any => {
+        const toParse = storage.getItem(key)
+        if (toParse === null || toParse === undefined) {
+            return {}
+        }
+        if (typeof toParse === 'string') {
+            return parseState(toParse)
+        }
+        if (typeof toParse.then === 'function') {
+            return toParse.then(parseState)
+        }
 
-  const getState = (): any => {
-    const toParse = storage.getItem(key)
-    if (toParse === null || toParse === undefined) {
-      return {}
-    }
-    if (typeof toParse === 'string') {
-      return parseState(toParse)
-    }
-    if (typeof toParse.then === 'function') {
-      return toParse.then(parseState)
+        return {}
     }
 
-    return {}
-  }
-
-  const parseState = (state: string) => {
-    if (state === undefined) {
-      return {}
+    const parseState = (state: string) => {
+        if (state === undefined) {
+            return {}
+        }
+        try {
+            return parser(state)
+        } catch (e) {
+            console.error(e)
+            return {}
+        }
     }
-    try {
-      return JSON.parse(state)
-    } catch (e) {
-      console.error(e)
-      return {}
-    }
-  }
 
-  const setState = (state: any): void => {
-    try {
-      if (typeof storage.mergeItem === 'function') {
-        storage.mergeItem(key, JSON.stringify(state))
-      } else {
-        storage.setItem(key, JSON.stringify(state))
-      }
-    } catch (e) {
-      console.error(e)
+    const setState = (state: any): void => {
+        try {
+            if (typeof storage.mergeItem === 'function') {
+                storage.mergeItem(key, stringify(state))
+            } else {
+                storage.setItem(key, stringify(state))
+            }
+        } catch (e) {
+            console.error(e)
+        }
     }
-  }
 
-  return { persistAtom }
+    return { persistAtom }
 }
